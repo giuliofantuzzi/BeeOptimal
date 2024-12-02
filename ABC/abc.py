5e835edf4d99869b182768d01b305ce1a22ca5ff
giuliofantuzzi <giulio.fantuzzi01@gmail.com>
2024-12-02 14:11:57 +0100

    Added DE/best mutations

--- ABC/abc.py
+++ ABC/abc.py
@@ -13,25 +13,27 @@ class ArtificialBeeColony():
     
     def __init__(self,n_bees,limit,function,bounds):
         assert ( n_bees > 2)
-        self.dim                    = len(bounds)
-        self.n_bees                 = n_bees
-        self.limit                  = limit if limit is not None else (self.n_bees //2)* self.dim
-        self.function               = function
-        self.bounds                 = bounds
-        self.n_employed_bees        = self.n_bees // 2
-        self.n_onlooker_bees        = self.n_bees - self.n_employed_bees
-        self.max_iters              = None
-        self.selection              = None
-        self.employed_bees          = []
-        self.onlooker_bees          = []
-        self.colony_history         = []
-        self.optimal_source         = ([np.nan for _ in range(self.dim)],np.nan)
-        self.optimal_source_history = []
+        self.dim                 = len(bounds)
+        self.n_bees              = n_bees
+        self.limit               = limit if limit is not None else (self.n_bees //2)* self.dim
+        self.function            = function
+        self.bounds              = bounds
+        self.n_employed_bees     = self.n_bees // 2
+        self.n_onlooker_bees     = self.n_bees - self.n_employed_bees
+        self.max_iters           = None
+        self.selection           = None
+        self.mutation            = None
+        self.employed_bees       = []
+        self.onlooker_bees       = []
+        self.colony_history      = []
+        self.optimal_bee         = []
+        self.optimal_bee_history = []
      
-    def optimize(self,max_iters=100,selection='RouletteWheel',verbose=False,random_seed=None):
+    def optimize(self,max_iters=100,selection='RouletteWheel',mutation='StandardABC',verbose=False,random_seed=None):
         
         self.max_iters = max_iters
         self.selection = selection
+        self.mutation  = mutation
         
         # Initialization
         if random_seed:
@@ -39,19 +41,18 @@ class ArtificialBeeColony():
         self.employed_bees = [Bee(position = None,
                                   function = self.function,
                                   bounds   = self.bounds) for _ in range(self.n_employed_bees) ]
-        self.optimal_source_history.append(copy.deepcopy(max(self.employed_bees,key=lambda bee: bee.fitness)))
         self.colony_history.append(copy.deepcopy(self.employed_bees))
+        self.optimal_bee = copy.deepcopy(max(self.employed_bees,key=lambda bee: bee.fitness))
+        self.optimal_bee_history.append(copy.deepcopy(self.optimal_bee))
         
         # Loop
         for _ in trange(self.max_iters,desc='Running Optimization',disable= not verbose):
             self.send_employees_()
             self.send_onlookers_()
             self.send_scouts_()
-            self.optimal_source_history.append(copy.deepcopy(max(self.employed_bees,key=lambda bee: bee.fitness)))
             self.colony_history.append(copy.deepcopy(self.employed_bees))
-
-        # Store best Solution
-        self.optimal_source = max(self.employed_bees,key=lambda bee: bee.fitness)
+            self.optimal_bee = copy.deepcopy(max(self.employed_bees,key=lambda bee: bee.fitness))
+            self.optimal_bee_history.append(copy.deepcopy(self.optimal_bee))
     
     def send_employees_(self):
         for bee_idx, bee in enumerate(self.employed_bees):
@@ -81,7 +82,6 @@ class ArtificialBeeColony():
             # Greedy Selection
             if candidate_bee.fitness >= bee.fitness:
                 self.onlooker_bees[bee_idx] = candidate_bee
-        
         # Update Employed Bees
         for bee_idx, bee in zip(dance_winners,self.onlooker_bees):
             if bee.fitness > self.employed_bees[bee_idx].fitness:
@@ -95,21 +95,43 @@ class ArtificialBeeColony():
                                                   bounds   = self.bounds)
                 
     def get_candidate_neighbor_(self,bee,bee_idx,population):
-        # Maybe it will be useful to define separate methods for different mutation (single bit, all bits probabilistically, etc)
-        # And here do a some cases depending on the mutation type -> standard ABC, DE/best/1, DE/best/2, ...
-        # Choose Donor Bee
-        while True:
-            k = np.random.randint(0,len(population))
-            if k != bee_idx: #nel caso abbia bisogno di più donor qui potrei usare k_list e mettere condizione aggiuntiva len(k_list)==n_donors
-                break
-        donor_bee = copy.deepcopy(population[k])
-        # Candidate Bee
-        j = np.random.randint(0,len(bee.position))
-        phi = np.random.uniform(-1,1)
-        candidate_bee = copy.deepcopy(bee)
-        candidate_bee.position[j] = bee.position[j] + phi*(bee.position[j] - donor_bee.position[j])
-        candidate_bee.position[j] = np.clip(candidate_bee.position[j],self.bounds[j][0],self.bounds[j][1])
+        
+        if self.mutation == 'StandardABC':
+            # Donor Bee
+            donor_bee = self.get_donor_bees_(n_donors=1,bee_idx=bee_idx,population=population)[0]
+            # Candidate Bee
+            j = np.random.randint(0,len(bee.position))
+            phi = np.random.uniform(-1,1)
+            candidate_bee = copy.deepcopy(bee)
+            candidate_bee.position[j] = bee.position[j] + phi*(bee.position[j] - donor_bee.position[j])
+            candidate_bee.position[j] = np.clip(candidate_bee.position[j],self.bounds[j][0],self.bounds[j][1])
+            
+        # Probabilistic mutation still to do (with MR parameter)
+            
+        if self.mutation == 'ABC/best/1':
+            # 2 Donor Bees
+            donor1,donor2 = self.get_donor_bees_(n_donors=2,bee_idx=bee_idx,population=population)
+            # Candidate Bee
+            j = np.random.randint(0,len(bee.position))
+            phi = np.random.uniform(-1,1)
+            candidate_bee = copy.deepcopy(bee)
+            candidate_bee.position[j] = self.optimal_bee.position[j] + phi*(donor1.position[j] - donor2.position[j])
+            candidate_bee.position[j] = np.clip(candidate_bee.position[j],self.bounds[j][0],self.bounds[j][1])
+            
+        if self.mutation == 'ABC/best/2':
+            # 4 Donor Bees
+            donor1,donor2,donor3,donor4 = self.get_donor_bees_(n_donors=4,bee_idx=bee_idx,population=population)
+            # Candidate Bee
+            j = np.random.randint(0,len(bee.position))
+            phi = np.random.uniform(-1,1)
+            candidate_bee = copy.deepcopy(bee)
+            candidate_bee.position[j] = self.optimal_bee.position[j] + phi*(donor1.position[j] - donor2.position[j]) + phi*(donor3.position[j] - donor4.position[j])
+            candidate_bee.position[j] = np.clip(candidate_bee.position[j],self.bounds[j][0],self.bounds[j][1])
         
         return candidate_bee
 
+    def get_donor_bees_(self,n_donors=1,bee_idx=None,population=None):
+        available_indices = np.delete(np.arange(len(population)), bee_idx)
+        k_list = np.random.choice(available_indices,size=n_donors,replace=False)
+        return [copy.deepcopy(population[k]) for k in k_list]
 #--------------------------------------------------------------------------------
--- test.py
+++ test.py
@@ -11,11 +11,11 @@ from PIL import Image
 #--------------------------------------------------------------------------------
 # Global variables and settings
 #--------------------------------------------------------------------------------
-N_BEES      = 50
+N_BEES      = 100
 LIMIT       = (N_BEES // 2) * 2
-MAX_ITERS   = 50
+MAX_ITERS   = 100
 BENCHMARK_FUNCTIONS = [Sphere,Rastrigin,Ackley,Eggholder]
-np.random.seed(1234) # Fix NumPy seed for reproducibility
+#np.random.seed(1234) # Fix NumPy seed for reproducibility
 #--------------------------------------------------------------------------------
 # Tests
 #--------------------------------------------------------------------------------
@@ -38,45 +38,45 @@ if __name__ == '__main__':
                                   bounds   = function_test.bounds,
                                   function = function_test.fun
                                 )
-        ABC.optimize(max_iters=MAX_ITERS,selection='RouletteWheel')
+        ABC.optimize(max_iters=MAX_ITERS,selection='RouletteWheel',mutation='ABC/best/2',verbose=False,random_seed=1234)
         
         print(f"Optimal Solution:")
         print(f"\tExpected : {function_test.optimal_solution}")
-        print(f"\tFound    : {ABC.optimal_source.position}")
+        print(f"\tFound    : {ABC.optimal_bee.position}")
         print(f"Optimal Value:")
         print(f"\tExpected : {function_test.optimal_value}")
-        print(f"\tFound    : {ABC.optimal_source.value}")
+        print(f"\tFound    : {ABC.optimal_bee.value}")
         print('-'*100)
         
-        # GIF
-        x = np.linspace(function_test.bounds[0][0]-0.05*np.abs(function_test.bounds[0][0]),
-                        function_test.bounds[0][1]+0.05*np.abs(function_test.bounds[0][1]), 
-                        100)
-        y = np.linspace(function_test.bounds[1][0]-0.05*np.abs(function_test.bounds[1][0]),
-                        function_test.bounds[1][1]+0.05*np.abs(function_test.bounds[1][1]),
-                        100)
-        X, Y = np.meshgrid(x, y)
-        Z = function_test.evaluate((X, Y))
+        # # GIF
+        # x = np.linspace(function_test.bounds[0][0]-0.05*np.abs(function_test.bounds[0][0]),
+        #                 function_test.bounds[0][1]+0.05*np.abs(function_test.bounds[0][1]), 
+        #                 100)
+        # y = np.linspace(function_test.bounds[1][0]-0.05*np.abs(function_test.bounds[1][0]),
+        #                 function_test.bounds[1][1]+0.05*np.abs(function_test.bounds[1][1]),
+        #                 100)
+        # X, Y = np.meshgrid(x, y)
+        # Z = function_test.evaluate((X, Y))
         
-        plots = []
-        for iteration,bee_colony in enumerate(ABC.colony_history):
-            plots.append(ContourPlotBee(x=x,y=y,Z=Z,bee_colony=bee_colony,
-                                        title=f"Iteration {iteration+1} / {ABC.max_iters}",
-                                        marker_path='assets/BeeMarker.png',
-                                        optimal_solution=function_test.optimal_solution))
+        # plots = []
+        # for iteration,bee_colony in enumerate(ABC.colony_history):
+        #     plots.append(ContourPlotBee(x=x,y=y,Z=Z,bee_colony=bee_colony,
+        #                                 title=f"Iteration {iteration+1} / {ABC.max_iters}",
+        #                                 marker_path='assets/BeeMarker.png',
+        #                                 optimal_solution=function_test.optimal_solution))
             
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            image_files = []
-            # Save each figure as a separate image file
-            for i, fig in enumerate(plots):
-                # Define the file path
-                file_path = f"{tmpdirname}/frame_{i}.png"
-                fig.write_image(file_path, format="png", scale=3)
-                image_files.append(file_path)
+        # with tempfile.TemporaryDirectory() as tmpdirname:
+        #     image_files = []
+        #     # Save each figure as a separate image file
+        #     for i, fig in enumerate(plots):
+        #         # Define the file path
+        #         file_path = f"{tmpdirname}/frame_{i}.png"
+        #         fig.write_image(file_path, format="png", scale=3)
+        #         image_files.append(file_path)
 
-            # Open images and save as GIF
-            images = [Image.open(file) for file in image_files]
-            gif_path = f"images/{function_test.name}_animated_opt.gif"
-            images[0].save(gif_path, save_all=True, append_images=images[1:], 
-                           duration=300, loop=0)
-            print(f"Animated GIF saved in {gif_path}")
+        #     # Open images and save as GIF
+        #     images = [Image.open(file) for file in image_files]
+        #     gif_path = f"images/{function_test.name}_animated_opt.gif"
+        #     images[0].save(gif_path, save_all=True, append_images=images[1:], 
+        #                    duration=300, loop=0)
+        #     print(f"Animated GIF saved in {gif_path}")
