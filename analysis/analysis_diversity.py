0a15654f837bd36a9f3555f357a10e651e084232
giuliofantuzzi <giulio.fantuzzi01@gmail.com>
2024-12-11 23:42:36 +0100

    Fixed title of diversity plots

--- analysis/analysis_diversity.py
+++ analysis/analysis_diversity.py
@@ -24,7 +24,7 @@ if __name__=="__main__":
         current_functions = [f"{function_name}-{dim}d" for dim in DIMS]
         current_df = diversity_df[diversity_df['Function'].isin(current_functions)]
         fig = px.box(current_df, x="Function", y="Diversity",color='Initialization')
-        fig.update_layout(title='Diversity of the population for different dimensions',
+        fig.update_layout(title='Diversity of the initial population for different dimensions',
                           width=800,height=500
                           )
         fig.write_image(f'{IMG_PATH}{function_name}.png',scale=3)
